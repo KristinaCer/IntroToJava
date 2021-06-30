5b656297e24b9cd34c32008b82d07e844c1d1aa2
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2021-06-30 09:39:15 +0200

    Added Retrieving data from file implementation

---
+++ java-basics/src/retrieveing_data_from_file/FileDemo.java
@@ -0,0 +1,31 @@
+package retrieveing_data_from_file;
+
+import java.io.*;
+
+public class FileDemo {
+    public static void main(String[] args) {
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(new File("log.txt"));
+            br = new BufferedReader(fr);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                stringBuilder.append(line);
+                /*
+                stringBuilder.append(line).append("n");
+                scores 0!!!!
+                 */
+            }
+            System.out.println(stringBuilder.toString());
+            fr.close();
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
