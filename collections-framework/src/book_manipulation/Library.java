e035e29ce7dbe45c986518c1015db26e9089e04c
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2022-04-20 17:21:38 +0200

    Update Library.java to be written using Stream API

--- collections-framework/src/book_manipulation/Library.java
+++ collections-framework/src/book_manipulation/Library.java
@@ -4,58 +4,35 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class Library {
-    ArrayList<Book> bookList = new  ArrayList<Book>();
+  ArrayList<Book> bookList = new ArrayList<>();
 
-    public ArrayList<Book> getBookList() {
-        return bookList;
-    }
+  public ArrayList<Book> getBookList() {
+    return bookList;
+  }
 
-    public void setBookList(ArrayList<Book> bookList) {
-        this.bookList = bookList;
-    }
-    public void addBook(Book bobj) {
-        this.bookList.add(bobj);
-    }
+  public void setBookList(ArrayList<Book> bookList) {
+    this.bookList = bookList;
+  }
 
-    public boolean isEmpty() {
-        return bookList.isEmpty();
-    }
+  public void addBook(Book bobj) {
+    this.bookList.add(bobj);
+  }
 
-    ArrayList<Book> viewAllBooks(){
-        return bookList;
-    }
+  public boolean isEmpty() {
+    return bookList.isEmpty();
+  }
 
-   /* ArrayList<Book> viewBooksByAuthor(String author ){
-        ArrayList <Book> byAuthor = new ArrayList<>();
-        for (Book book : this.bookList){
-            if (book.getAuthor().equals(author)) {
-                byAuthor.add(book);
-            }
-        }
-        return byAuthor;
-    } */
+  ArrayList<Book> viewAllBooks() {
+    return bookList;
+  }
 
+  ArrayList<Book> viewBooksByAuthor(String author) {
+    return bookList.stream()
+        .filter(book -> book.getAuthor().equals(author))
+        .collect(Collectors.toCollection(ArrayList::new));
+  }
 
-
-    ArrayList<Book> viewBooksByAuthor(String author ) {
-        return this.bookList.stream()
-                .filter(book -> book.getAuthor().equals(author))
-                .collect(Collectors.toCollection(ArrayList::new));
-    }
-
-       /* int countnoofbook(String bname) {
-        int count = 0;
-        for (Book book : this.bookList) {
-            if (book.getBookName().equals(bname)) {
-                count++;
-            }
-        }
-        return count;
-    } */
-
-    int countnoofbook(String bname) {
-        return (int) this.bookList.stream()
-                .filter(book -> book.getBookName().equals(bname))
-                .count();
-    }
+  int countnoofbook(String bname) {
+    return (int) bookList.stream().filter(book -> book.getBookName().equals(bname)).count();
+  }
 }
